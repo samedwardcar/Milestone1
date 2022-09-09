6597ea8b3ced378e80c5ad65511813b17b3f0a73
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-09-08 20:45:07 -0500

    testing method I want to use to get board set up.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Milestone1
 First milestone project, recreating the filler pigeon iPhone game.
+
+Started with playing the game to see the different elements and functions I would need to use.
--- filler.js
+++ filler.js
@@ -1,1 +1,11 @@
+const gameBoarder = document.getElementById("board");
+let rows = document.getElementsByClassName("gridRow");
+let cell = document.getElementsByClassName("cell");
+
+function defaultBoard(){
+    madeRows(7);
+    makeColumns(8);
+}
+
+
 
