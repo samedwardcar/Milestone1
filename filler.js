6e1065961c70ca80a5b07d630bd6d58a3011f923
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-09-11 20:58:51 -0500

    starting javascript code to add random gameboard tile color generator

--- filler.js
+++ filler.js
@@ -1,11 +1,16 @@
-const gameBoarder = document.getElementById("board");
-let rows = document.getElementsByClassName("gridRow");
-let cell = document.getElementsByClassName("cell");
+// const gameBoarder = document.getElementById("board");
+// let rows = document.getElementsByClassName("gridRow");
+// let cell = document.getElementsByClassName("cell");
 
 // function defaultBoard(){
 //     madeRows(7);
 //     makeColumns(8);
 // }
 
+// var colors = ["red", "green", "yellow", "blue", "purple", "black"];
+// var tile1 = document.getElementsById("tile1");
+// var rand = Math.floor(Math.random()*colors.length);
+// tile1.style.backGroundColor = colors[rand];
+var colors = ["red", "green", "yellow", "blue", "purple", "black"];
 
-
+tile1.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
