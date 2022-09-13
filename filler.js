0e726829de09851445857c588a142e71a3979194
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-09-12 21:14:41 -0500

    random color selector

--- filler.js
+++ filler.js
@@ -1,3 +1,12 @@
+var gameBoard = document.querySelectorAll('.tile');
+
+for(i = 0; i< gameBoard.length; i++) {
+let colors = ["red", "green", "yellow", "blue", "purple", "black"];
+tile32.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+}
+
+
+
 // const gameBoarder = document.getElementById("board");
 // let rows = document.getElementsByClassName("gridRow");
 // let cell = document.getElementsByClassName("cell");
@@ -11,6 +20,4 @@
 // var tile1 = document.getElementsById("tile1");
 // var rand = Math.floor(Math.random()*colors.length);
 // tile1.style.backGroundColor = colors[rand];
-var colors = ["red", "green", "yellow", "blue", "purple", "black"];
 
-tile1.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
