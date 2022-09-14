a0e7e6c5e3a2d5f64e11b2456bb73b0490b3b878
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-09-13 20:53:59 -0500

    Set gameboard

--- filler.js
+++ filler.js
@@ -1,23 +1,15 @@
-var gameBoard = document.querySelectorAll('.tile');
+const colors = ["red", "green", "yellow", "blue", "purple", "black"];
+let gameBoard = document.querySelectorAll('.tile');
 
-for(i = 0; i< gameBoard.length; i++) {
-let colors = ["red", "green", "yellow", "blue", "purple", "black"];
-tile32.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+let gameBoardTile = [gameBoard[0]]
+
+for(i = 0; i< gameBoard.length; i++){
+    gameBoard[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 }
 
-
-
-// const gameBoarder = document.getElementById("board");
-// let rows = document.getElementsByClassName("gridRow");
-// let cell = document.getElementsByClassName("cell");
-
-// function defaultBoard(){
-//     madeRows(7);
-//     makeColumns(8);
-// }
 
 // var colors = ["red", "green", "yellow", "blue", "purple", "black"];
 // var tile1 = document.getElementsById("tile1");
 // var rand = Math.floor(Math.random()*colors.length);
 // tile1.style.backGroundColor = colors[rand];
-
+// 
